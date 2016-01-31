6a2895a149f2a10b27bba5d772843320a588c3cc
Abhishek Kumar <akbittu@gmail.com>
2016-01-31 14:14:36 +0530

    The complete sample

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+Hindi Web Input by Abhishek Kumar is licensed under a Creative Commons Attribution 4.0 International License.
+Based on a work at https://github.com/isurfer21/Hindi-Web-Input.
+*/
+
+window.onload = function () {
+    var it_demo = document.getElementById('it_demo');
+    it_demo.value = 'jkr rd jwe ij jqd tk';
+
+    var ta_demo = document.getElementById('ta_demo');
+    ta_demo.value = 'jkr rd jwe ij jqd tk';
+
+    var dv_demo = document.getElementById('dv_demo');
+    dv_demo.innerHTML = 'jkr rd jwe ij jqd tk';
+};
